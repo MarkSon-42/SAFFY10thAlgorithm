bfc10da26d153b1bc223c5bf374e8bc7b54a4d03
minwoo <mark42son@gmail.com>
2023-05-12 19:54:53 +0900

    solved as chapgpt..

--- Minwoo/햄버거 다이어트.py
+++ Minwoo/햄버거 다이어트.py
@@ -1,28 +1,6 @@
-# 문제를 이해 못함
+t = int(input())
+for _ in range(t):
+    num, limit = map(int, input().split())
+    for _ in range(num):
+        taste, kcal = map(int, input().split())
 
-# T = int(input())
-# for _ in range(T):
-#     bits = input()
-#     cnt = 0
-#     for b in bits:
-#         if b != '0':
-#             bits[b:] = '1'
-#             cnt += 1
-#         else:
-#             continue
-#     print(cnt)
-
-
-T = int(input())
-
-for i in range(1, T+1):
-    cnt = 0
-    m = list(input())
-    origin = len(m) * ['0']
-    for j in range(len(m)):
-        if m[j] != origin[j]:
-            for k in range(j, len(m)):
-                origin[k] = m[j]
-            cnt += 1
-
-    print("#{} {}".format(i, cnt))
