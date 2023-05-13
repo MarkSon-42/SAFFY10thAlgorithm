c90f52eea3ad9042a94755d97120bfc2a5796954
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2023-05-13 20:55:58 +0900

    5/13

--- Jongwon/0513/5215.py
+++ Jongwon/0513/5215.py
@@ -1,2 +1,17 @@
 # 5215. 햄버거 다이어트
 
+T = int(input())
+
+for test_case in range(1, T+1):
+    N, L = map(int, input().split())
+    
+    ingredient = []
+    for _ in range(N):
+        T, K = map(int, input().split())
+        ingredient.append((T, K))
+
+    combination = [[] for _ in range(N)] # 점수와 칼로리 조합 
+
+    # L을 넘지 않은 모든 조합을 combination에 저장
+
+    # 그 중에서 가장 큰 점수 추출
