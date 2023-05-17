80e4a46fe644a8a206a2985b4177443bca3283f3
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2023-05-17 20:57:51 +0900

    5/17

--- Jongwon/0517/5789(제한시간초과).py
+++ Jongwon/0517/5789(제한시간초과).py
@@ -4,10 +4,10 @@ T = int(input())
 for test_case in range(1, T+1):
     N, Q = map(int, input().split())
     zero_arr = [0 for _ in range(N)]
-    for i in range(Q):
+    for i in range(1, Q+1):
         L, R = map(int, input().split())
         for j in range(L-1, R):
-            zero_arr[j] = i+1
+            zero_arr[j] = i
     
     ans = ' '.join(map(str, zero_arr))
 
