a3a0c2122dfa9106e6c19115a796262eea72595c
minwoo <mark42son@gmail.com>
2023-05-18 21:05:51 +0900

    code push

--- Minwoo/0518/구구단1.py
+++ Minwoo/0518/구구단1.py
@@ -1,11 +1,15 @@
 TC = int(input())
 
 for i in range(TC):
-    N = int(input())
-    answer = ''
-
+    gugu_list = []
     for j in range(1, 10):
-
-
+        for k in range(1, 10):
+            gugu_list.append(j*k)
+    gugu_list = set(gugu_list) # 굳이 할 필요 없는데 그냥 해봄
+    N = int(input())
+    if N in gugu_list:
+        answer = "Yes"
+    else:
+        answer = "No"
 
     print("#{} {}".format(i + 1, answer))
